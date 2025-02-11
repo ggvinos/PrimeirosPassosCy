e9c8da667b5071529e91199f0f010a42f162c085
ggvinos <vincios1901@gmail.com>
2025-02-11 20:38:10 -0300

    First commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,27 @@
+describe('template spec', () => {
+  it('login - Success', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type("Admin")
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type("admin123")
+    cy.get('.oxd-button').click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
+  })
+
+
+  it.only('login - fail', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type("Admim")
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type("admin123")
+    cy.get('.oxd-button').click()
+    cy.get('.oxd-alert')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type("Admin")
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type("admin123")
+    cy.get('.oxd-button').click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text')
+  })
+
+
+  
+})

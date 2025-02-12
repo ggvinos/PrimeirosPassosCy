cbb7e4fe4283ce10c349658d0fc1e5204909ae32
ggvinos <vincios1901@gmail.com>
2025-02-12 19:52:00 -0300

    addin user data json

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,33 +1,38 @@
+
+import userData from '../fixtures/user-data.json'
 describe('template spec', () => {
 
-  const selectorList = {
-    usernameField: "[name='username']",
+  const selectorList = {  //objeto que armazena seletores CSS dos elementos da página
+    usernameField: "[name='username']", 
     passwordField: "[name='password']",
     loginButton: "[type='submit']",
     sectionTitleTopBar : '.oxd-topbar-header-breadcrumb > .oxd-text',
+    dashboardGrid: ".orangehrm-dashboard-grid",
     wrongCredentialAlert: "[role='alert']"
-  }
+  } 
+  
+  //A ideia é centralizar os seletores para facilitar a manutenção e evitar repetição
   it('login - Success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorList.usernameField).type("Admin")
-    cy.get(selectorList.passwordField).type("admin123")
+    cy.get(selectorList.usernameField).type(userData.userSuccess.username)
+    cy.get(selectorList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorList.sectionTitleTopBar).contains('Dashboard')
+    cy.get(selectorList.dashboardGrid)
   })
 
 
   it('login - fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorList.usernameField).type("Admim")
-    cy.get(selectorList.passwordField).type("admin123")
+    cy.get(selectorList.usernameField).type(userData.userFail.username)
+    cy.get(selectorList.passwordField).type(userData.userFail.password)
     cy.get(selectorList.loginButton).click()
     cy.get('.oxd-alert')
-    cy.get(selectorList.usernameField).type("Admin")
-    cy.get(selectorList.passwordField).type("admin123")
+    cy.get(selectorList.usernameField).type(userData.userSuccess.username)
+    cy.get(selectorList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorList.sectionTitleTopBar).contains('Dashboard')
+    cy.get(selectorList.dashboardGrid)
   })
 
 

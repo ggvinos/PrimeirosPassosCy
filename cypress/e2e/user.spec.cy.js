75f475542b523cd33a4d2878f517bc4fd45ecd18
ggvinos <vincios1901@gmail.com>
2025-02-13 19:56:58 -0300

    added challenge comboBox

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -15,6 +15,8 @@ describe('template spec', () => {
     genericField: "[data-v-1f99f73c='']",
     dataCloseButton: ".--close",
     submitButton: "[type='submit']",
+    comboBoxGeneric: "[data-v-67d2aedf=''][data-v-bddebfba='']",
+
     
   } 
   
@@ -35,9 +37,18 @@ describe('template spec', () => {
     cy.get(selectorList.genericField).eq(4).clear().type("EmployeID")
     cy.get(selectorList.genericField).eq(5).clear().type("Other Id TEST")
     cy.get(selectorList.genericField).eq(6).clear().type("DriversLicenseTEST")
+
+    cy.get(selectorList.comboBoxGeneric).eq(0).click()
+    cy.get(':nth-child(6) > span').click()
+    cy.get(selectorList.comboBoxGeneric).eq(1).click()
+    cy.get(':nth-child(3) > span').click()
+    
     cy.get(selectorList.genericField).eq(7).clear().type("2020-10-10")
     cy.get(selectorList.dataCloseButton).click
+   
     cy.get(selectorList.genericField).eq(9).clear().type("Test_Field")
+  
+
     cy.get(selectorList.submitButton).eq(0).click()
     cy.get('body').should('contain', 'Successfully Updated')
     cy.get('.oxd-toast-close').click()
